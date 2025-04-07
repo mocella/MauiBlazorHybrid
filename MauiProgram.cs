b60c4d7ecdf228565c34d27bf8f15b81570a7010
Tony Mocella <Tony.Mocella@neudesic.com>
2025-04-07 08:45:35 -0500

    initial commit

---
+++ MauiProgram.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace MauiBlazorHybrid
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMudServices(config =>
+            {
+
+                // SnackBar Configuration
+                // NOTE: did not find a global option to turn off the Icon, but you can do that in the individual Snackbar call :( 
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = true;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 5000; // 5 seconds (default)
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
